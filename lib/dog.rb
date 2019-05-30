5a26d9e18f06ab8653d41ac09bbfa6b99cc05be5
craig wadding <craig.wadding@gmail.com>
2019-05-30 03:00:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,18 @@
 class Dog 
   
-  def initialize(name, breed)
-    @name = name 
-    @breed = breed 
+  def name 
+    @name 
+  end 
+  
+  def name=(new_name)
+    @name = new_name
+  end 
+  
+  def breed   
+    @breed
   end
+  
+  def breed=(new_breed) 
+    @breed = new_breed
+  end 
 end 
